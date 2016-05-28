fa8a641b713401b1827aa484103148e148346af6
vgrem <vvgrem@gmail.com>
2016-05-28 14:44:48 +0300

    Disabled (temporarily) support for unit testing against hhvm runtime

--- tests/ListTest.php
+++ tests/ListTest.php
@@ -10,7 +10,7 @@ class ListTest extends SharePointTestCase
 {
     public function testIfListCreated()
     {
-        $listTitle = "Orders_" . rand(1,1000);
+        $listTitle = "Orders_" . rand(1,100000);
         $list = TestUtilities::ensureList($this->context,$listTitle,\SharePoint\PHP\Client\ListTemplateType::Tasks);
         $this->assertEquals($list->getProperty('Title'),$listTitle);
         return $list;
@@ -22,8 +22,25 @@ class ListTest extends SharePointTestCase
      */
     public function testDeleteList(\SharePoint\PHP\Client\SPList $listToDelete)
     {
-        $listToDelete->deleteObject();
-        $this->context->executeQuery();
+        $listId = $listToDelete->getProperty('Id');
+        $ctx = $listToDelete->getContext();
+        $lists = $ctx->getWeb()->getLists();
+        $ctx->load($lists);
+        $ctx->executeQuery();
+
+        $listsCount = $lists->getCount();
+        $result = array_filter(
+            $lists->getData(),
+            function ($l) use ($listId) {
+                return $l->getProperty('Id') == $listId;
+            }
+        );
+
+        if(count($result)  == 1){
+            $listToDelete->deleteObject();
+            $ctx->load($lists);
+            $ctx->executeQuery();
+        }
         
         
     }
