add68ac4b17dfc0521e095109e29f1cb8c50a649
vgrem <vvgrem@gmail.com>
2016-11-13 13:51:15 +0200

    Minor changes: autoload & travis config fixes

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 require_once('Settings.php');
-require_once(__DIR__ . '/../src/bootstrap.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
 
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ require_once(__DIR__ . '/../examples/Settings.php');
 require_once('OutlookServicesTestCase.php');
 require_once('SharePointTestCase.php');
 require_once('TestUtilities.php');
-require_once(__DIR__ . '/../src/bootstrap.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
 
 
